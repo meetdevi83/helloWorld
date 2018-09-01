828fda6211ce9492a0d0b5999d1ee878126d2a7b
meetdevi83 <meetdevi83@gmail.com>
2018-09-01 20:40:24 +0100

    chg

--- src/test/java/TestHello.java
+++ src/test/java/TestHello.java
@@ -8,6 +8,10 @@ public class TestHello {
         Assert.assertEquals(0,0);
         //dir comment
         //hai
+<<<<<<< HEAD
         //command 1.3
+=======
+        //command 1.1
+>>>>>>> parent of e8573bd... 1.2 version
     }
 }
